a19ebce07058c09952cbcdd17183b383bcb1c165
AmangeldiyevSultan <amangeldievsultan2003@mail.ru>
2023-01-11 02:09:06 +0600

    rating,products, etc

---
+++ server/routes/product.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const auth = require('../middlewares/auth');
+const Product = require('../models/product');  
+const productRouter = express.Router();
+
+// /api/product?category=Essential
+// req.query.category
+
+// /api/product:category=Essential
+// req.params.category
+ 
+
+productRouter.get("/api/products/", auth, async (req, res) => {
+    try{ 
+        const product = await Product.find({category: req.query.category});
+        res.json(product);   
+    } catch (e) {
+        res.status(500).json({error: e.message});  
+    }
+});
+
+// /api/product/search/i
+
+// /api/product/search/:hello/:greate
+//                             req.params.greate
+
+productRouter.get("/api/products/search/:name", auth, async (req, res) => {
+    try{ 
+
+        const productSearch = await Product.find({
+            name: {$regex: req.params.name, $options: "i"},
+        }); 
+        // Ex  
+        // iphone
+        // ip (it also find with a help regex and options)
+        res.json(productSearch); 
+    } catch (e){
+        res.status(500).json({error: e.message});
+    }
+});
+
+// create post req
+productRouter.post('/api/rate-product', auth, async (req, res)=> {
+    try{
+        const {id, rating} = req.body;
+        let product = await Product.findById(id)
+
+        for(let i = 0; i < product.ratings.length; i++){
+            if(product.ratings[i].userId == req.user){
+                product.ratings.splice(i , 1);
+                break;
+            }
+        }
+
+        const ratingSchema = {
+            userId: req.user,
+            rating
+        };
+
+        product.ratings.push(ratingSchema);
+        product = await product.save();
+        res.json(product);  
+
+    } catch (e){
+        res.status(500).json({error: e.message});
+    }
+});
+
+module.exports = productRouter;
